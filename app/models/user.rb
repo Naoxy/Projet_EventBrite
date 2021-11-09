72716d2e443f760566e2cfc9fa938f7c22d6df64
Naoxys <souleyk3@gmail.com>
2021-11-09 22:07:46 +0100

    modif

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ApplicationRecord
-  belongs_to :event, througth: :attendances 
-
-  after_create :welcome_send
-
-  def welcome_send
-    UserMailer.welcome_email(self).deliver_now
-  end
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
