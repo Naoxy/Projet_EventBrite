078f98cfcbaae5c6ae9992044c85e3b38b4c6c04
Naoxys <souleyk3@gmail.com>
2021-11-11 01:18:45 +0100

    commit

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,14 +1,33 @@
 class Event < ApplicationRecord
-  belongs_to :user 
+  belongs_to :admin, class_name: "User"
   has_many :attendances
-  has_many :users, througth: :attendances
+  has_many :users, through: :attendances
   
 
-  validates :duration, presence: true
+  validates :duration, presence: true 
   validates :title, presence: true, length: { in: 5..140 }
   validates :description, presence: true, length: { in: 20..1000 }
   validates :price, presence: true, inclusion: { in: 1..1000 }
   validates :location, presence: true
+  validate :duration_valid?
+  validate :past?
+
+
+  private
+
+  def duration_valid?
+    if duration.positive? && (duration % 5).zero?
+      errors.add(:duration, 'la durée doit etre positive et incrémentée de 5 en 5')
+    end
+  end
+
+  def past?
+    return false if start_date.nil?
+  if start_date > Time.zone.now
+    errors.add(:start_date, 'la date ne doit pas etre dans le passé')
+  end
+end
+
 end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :event_admin, foreign_key: "admin_id", class_name: "Event"
+  has_many :attendances
+  has_many :events, through: :attendances
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
